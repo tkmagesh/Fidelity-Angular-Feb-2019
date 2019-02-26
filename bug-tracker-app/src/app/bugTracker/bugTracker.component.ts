5ad48a012d893ec94af889d02fa123e9062808c6
Magesh <tkmagesh77@gmail.com>
2019-02-26 16:15:02 +0530

    adding, listing & toggling bugs

---
+++ bug-tracker-app/src/app/bugTracker/bugTracker.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { Bug } from './models/Bug';
+
+@Component({
+    selector : 'app-bug-tracker',
+    templateUrl : 'bugTracker.component.html'
+})
+export class BugTrackerComponent{
+    bugs : Bug[] = [];
+
+    onAddNewClick(newBugName : string){
+        let newBug : Bug = {
+            name : newBugName,
+            isClosed : false
+        };
+        this.bugs.push(newBug);
+    }
+
+    onBugNameClick(bug : Bug){
+        bug.isClosed = !bug.isClosed;
+    }
+}
