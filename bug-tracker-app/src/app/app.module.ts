a1a8a87e9827a261b82e3a05c43b3930f4424e1b
Magesh <tkmagesh77@gmail.com>
2019-02-27 14:31:48 +0530

    refactoring to bugStats and closedCountPipe

--- bug-tracker-app/src/app/app.module.ts
+++ bug-tracker-app/src/app/app.module.ts
@@ -5,15 +5,19 @@ import { UtilsModule } from './utils/utils.module';
 
 import { BugOperationsService } from './bugTracker/services/bugOperations.service';
 import { BugStorageService } from './bugTracker/services/bugStorage.service';
+import { ClosedCountPipe } from './bugTracker/pipes/closedCount.pipe'
 
 import { AppComponent } from './app.component';
 import { BugTrackerComponent } from './bugTracker/bugTracker.component';
 
+import { BugStatsComponent } from './bugTracker/views/bugStats.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BugTrackerComponent
+    BugTrackerComponent,
+    BugStatsComponent,
+    ClosedCountPipe
   ],
   imports: [
     BrowserModule
