b2188d10f6dd4c944449931eec14be5bb673dd92
Magesh <tkmagesh77@gmail.com>
2019-02-27 15:05:15 +0530

    refactoring to bugEdit component

---
+++ bug-tracker-app/src/app/bugTracker/views/bugEdit.component.ts
@@ -0,0 +1,28 @@
+import { Component, EventEmitter, Output } from "@angular/core";
+import { Bug } from '../models/Bug';
+import { BugOperationsService } from '../services/bugOperations.service';
+
+@Component({
+    selector : 'app-bug-edit',
+    template : `
+        <section class="edit">
+        <label for="">Bug Name :</label>
+        <input type="text" #txtBugName>
+        <input type="button" value="Add New" (click)="onAddNewClick(txtBugName.value)">
+    </section>
+    `
+})
+export class BugEditComponent{
+
+    @Output()
+    bugAdded : EventEmitter<Bug> = new EventEmitter<Bug>();
+    
+    constructor(private bugOperations : BugOperationsService){
+    }
+    onAddNewClick(newBugName : string){
+        let newBug : Bug = this.bugOperations.createNew(newBugName);
+        //this.bugs.push(newBug);
+        this.bugAdded.emit(newBug);
+    }
+
+}
